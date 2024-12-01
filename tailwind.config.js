e080d875a74101fa6fc0b1358655be62d98ecf7a
FacundoCien <facundocuentayt@gmail.com>
2024-11-30 21:14:32 -0500

    test

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}",  // Rutas de tus archivos Astro, HTML y JS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
